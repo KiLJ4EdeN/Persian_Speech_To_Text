4d00c283ff31208668ab1f722139187c795be1a1
Abdolkarim Saeedi <parsa592323@gmail.com>
2020-01-16 08:41:24 +0330

    Update Persian_Speech_To_Text.py

--- Persian_Speech_To_Text/Persian_Speech_To_Text.py
+++ Persian_Speech_To_Text/Persian_Speech_To_Text.py
@@ -7,9 +7,27 @@ Created on Thu Oct 10 09:27:24 2019
 
 import speech_recognition as sr
 
+# load desired audio file
+# or use mic to do recognition simultaneously
+
 filename = sr.AudioFile('myfile.wav')
+
+# create a recognizer instance
+
 r = sr.Recognizer()
+
+# open the audio file
+
 with filename as source:
+    
+    # denoise the file for better precision
+    
     r.adjust_for_ambient_noise(source)
+    
+    # read from data
+    
     audio = r.record(source)
+    
+# perform recognition and see the results
+
 print(r.recognize_google(audio, language = 'fa-IR', show_all = True ))
